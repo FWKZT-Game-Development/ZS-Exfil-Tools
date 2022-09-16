fc7786f0976d748d092a4dfabad524eac25fd6bd
Bagel™ <djmiller03@twcny.rr.com>
2022-09-16 14:25:18 -0400

    Added to toolgun

--- zs_exfil_tool/lua/autorun/client/cl_tool.lua
+++ zs_exfil_tool/lua/autorun/client/cl_tool.lua
@@ -1,10 +1,15 @@
 local start_pos = vector_origin
 local end_pos
-local storage = {}
+
+ExfilStorage = ExfilStorage or {}
+ExfilCount = ExfilCount or 0
+
+local selected = CreateConVar("exfil_select", "", FCVAR_USERINFO)
+local show = CreateConVar("exfil_show", "0", FCVAR_ARCHIVE + FCVAR_NOTIFY)
 
 hook.Add( "InitPostEntity", "Exfil.CreateToolTable", function()
-    if GAMEMODE_NAME == "sandbox" then
-        storage[ game.GetMap() ] = {}
+    if GAMEMODE_NAME == "sandbox" or GAMEMODE_NAME == "zombiesurvival" then
+        ExfilStorage[ game.GetMap() ] = {}
 
         --Load json data so we can let devs view positions.
         local files, dirs = file.Find( "exfil/*", "DATA" )
@@ -15,7 +20,8 @@ hook.Add( "InitPostEntity", "Exfil.CreateToolTable", function()
 
                 for i, data in pairs( stored_mapdata ) do
                     for area, d in pairs( data ) do
-                        table.insert( storage[game.GetMap()], d )
+                        table.insert( ExfilStorage[game.GetMap()], d )
+                        ExfilCount = ExfilCount + 1
                     end
                 end
             end
@@ -24,7 +30,7 @@ hook.Add( "InitPostEntity", "Exfil.CreateToolTable", function()
 end )
 
 local function ExfilDataHasArea(stage)
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == stage then
                 return true
@@ -47,36 +53,118 @@ local function CreateMapData(area)
             UseHatch = 0
         } 
     }
-    table.insert( storage[game.GetMap()], data )
+    table.insert( ExfilStorage[game.GetMap()], data )
 end
+
+local closest = nil
+
+if CLIENT then
+    hook.Add("Think", "ExfilToolgunGetClosest", function()
+        if not show:GetBool() then return end
+        local bestDist = math.huge
+        if ExfilStorage[ game.GetMap() ] then
+            for area, data in pairs( ExfilStorage[ game.GetMap() ] ) do
+                for area, stored in pairs( data ) do
+                    local calc = (stored.Pos + stored.BoxSize / 2):DistToSqr(LocalPlayer():GetPos())
+                    if calc < bestDist then
+                        bestDist = calc
+                        closest = area
+                    end
+                end
+            end
+        end
+    end)
+end
+
+cvars.AddChangeCallback("exfil_select", function(convar, oldValue, newValue)
+    if newValue == "" then
+        GetConVar("exfil_px"):SetFloat(0)
+        GetConVar("exfil_py"):SetFloat(0)
+        GetConVar("exfil_pz"):SetFloat(0)
+        GetConVar("exfil_override"):SetInt(0)
+        GetConVar("exfil_slaydelay"):SetFloat(1)
+        GetConVar("exfil_time"):SetFloat(EXFIL_TIME or 7)
+        GetConVar("exfil_deadline"):SetFloat(120)
+        GetConVar("exfil_usehatch"):SetInt(0)
+        return
+    end
+    if ExfilStorage[ game.GetMap() ] then
+        for area, data in pairs( ExfilStorage[ game.GetMap() ] ) do
+            for area, stored in pairs( data ) do
+                if area == newValue then
+                    GetConVar("exfil_px"):SetFloat(stored.Pos.x)
+                    GetConVar("exfil_py"):SetFloat(stored.Pos.y)
+                    GetConVar("exfil_pz"):SetFloat(stored.Pos.z)
+                    GetConVar("exfil_override"):SetInt(stored.OverrideExfilBool or 0)
+                    GetConVar("exfil_slaydelay"):SetFloat(stored.ZombieSlayDelay)
+                    GetConVar("exfil_time"):SetFloat(stored.ExfilTime or EXFIL_TIME or 7)
+                    GetConVar("exfil_deadline"):SetFloat(stored.ExfilDeadline)
+                    GetConVar("exfil_usehatch"):SetInt(stored.UseHatch or 0)
+                end
+            end
+        end
+    end
+end)
+
+concommand.Add("exfil_apply", function(ply, cmd, args)
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
+        for area, stored in pairs( data ) do
+            if area == selected:GetString() then
+                stored.OverrideExfilBool = GetConVar("exfil_override"):GetInt()
+                stored.ZombieSlayDelay = GetConVar("exfil_slaydelay"):GetFloat()
+                stored.ExfilTime = GetConVar("exfil_time"):GetFloat()
+                stored.ExfilDeadline = GetConVar("exfil_deadline"):GetFloat()
+                stored.UseHatch = GetConVar("exfil_usehatch"):GetInt()
+            end
+        end
+    end
+end)
+
+concommand.Add("exfil_select_closest", function(ply, cmd, args)
+    if closest then
+        if selected:GetString() == closest and not GetConVar("exfil_menu"):GetBool() then
+            selected:SetString("")
+            return
+        end
+        GetConVar("exfil_menu"):SetBool(false)
+        selected:SetString(closest)
+    end
+end)
 
 --example : exfil_create testarea
 concommand.Add("exfil_create", function( ply, cmd, args )
+    args[1] = args[1] or "exfil" .. ExfilCount + 1
     if not ExfilDataHasArea(args[1]) then
         CreateMapData( args[1] )
+        ExfilCount = ExfilCount + 1
     else
         ply:ChatPrint("[EXFIL] error : Map data has already been created!")
     end
-    PrintTable(storage)
+    PrintTable(ExfilStorage)
+    selected:SetString(args[1])
 end)
 
 --example : exfil_delete testarea
 concommand.Add("exfil_delete", function( ply, cmd, args )
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    args[1] = args[1] or selected:GetString()
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == args[1] then
-                table.remove( storage[ game.GetMap() ], _ )
+                table.remove( ExfilStorage[ game.GetMap() ], _ )
+                ExfilCount = ExfilCount - 1
+                LocalPlayer():ConCommand("exfil_select \"\"")
             end
         end
     end
-    PrintTable(storage)
+    PrintTable(ExfilStorage)
 end)
 
 --example : exfil_origin testarea
 concommand.Add("exfil_origin", function( ply, cmd, args )
+    args[1] = args[1] or selected:GetString()
     start_pos = ply:GetEyeTrace().HitPos
 
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == args[1] then
                 stored.Pos = start_pos
@@ -84,12 +172,13 @@ concommand.Add("exfil_origin", function( ply, cmd, args )
         end
     end
 
-    PrintTable(storage)
+    --PrintTable(ExfilStorage)
 end)
 
 --example : exfil_pos testarea 10 10 10
 concommand.Add("exfil_pos", function( ply, cmd, args )
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    args[1] = args[1] or selected:GetString()
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == args[1] then
                 stored.Pos = Vector( args[2], args[3], args[4] )
@@ -97,13 +186,14 @@ concommand.Add("exfil_pos", function( ply, cmd, args )
         end
     end
 
-    PrintTable(storage)
+    --PrintTable(ExfilStorage)
 end)
 
 --example : exfil_size testarea 200 200 200
 concommand.Add("exfil_size", function( ply, cmd, args )
+    args[1] = args[1] or selected:GetString()
     end_pos = Vector( args[2], args[3], args[4] )
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == args[1] then
                 stored.BoxSize = end_pos
@@ -111,12 +201,13 @@ concommand.Add("exfil_size", function( ply, cmd, args )
         end
     end
 
-    PrintTable(storage)
+    --PrintTable(ExfilStorage)
 end)
 
 --example : exfil_config testarea 0 3 5 120 0
 concommand.Add("exfil_config", function( ply, cmd, args )
-    for _, data in ipairs( storage[ game.GetMap() ] ) do
+    args[1] = args[1] or selected:GetString()
+    for _, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
         for area, stored in pairs( data ) do
             if area == args[1] then
                 stored.OverrideExfilBool = args[2]
@@ -128,27 +219,44 @@ concommand.Add("exfil_config", function( ply, cmd, args )
         end
     end
 
-    PrintTable(storage)
+    PrintTable(ExfilStorage)
 end)
 
 --example : exfil_save
 concommand.Add("exfil_save", function( ply, cmd, args )
-    local tab = util.TableToJSON( storage )
+    local tab = util.TableToJSON( ExfilStorage )
     file.CreateDir( "exfil" )
-    file.Write( "exfil/"..game.GetMap()..".json", tab)
+    file.Write( "exfil/" .. game.GetMap() .. ".json", tab)
+
+    PrintTable(ExfilStorage)
 
     ply:ChatPrint("[EXFIL] Map data has now been saved.")
 end)
 
+concommand.Add("exfil_print", function(ply, cmd, args)
+    PrintTable(ExfilStorage)
+end)
+
 hook.Add( "PostDrawTranslucentRenderables", "ZS.ExfilTool", function( bDepth, bSkybox )
+    if not show:GetBool() then return end
     if ( bSkybox ) then return end
-    if storage[ game.GetMap() ] then
-        for area, data in pairs( storage[ game.GetMap() ] ) do
+    if ExfilStorage[ game.GetMap() ] then
+        for area, data in pairs( ExfilStorage[ game.GetMap() ] ) do
             for area, stored in pairs( data ) do
-                if stored.Pos ~= nil then
+                if stored.Pos ~= nil and GetConVar("exfil_select"):GetString() ~= area then
+                    local oc = Color(161, 255, 156)
+                    
+                    if area == closest then
+                        oc = Color(255, 255, 0)
+                    end
+
+                    local r = oc.r
+                    local g = oc.g
+                    local b = oc.b
+                    
                     render.SetColorMaterial()
-                    render.DrawBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( 161, 255, 156, math.floor( math.sin( CurTime() * 8 ) * 5 ) + 10 ) )
-                    render.DrawWireframeBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( 161, 255, 156 ), false )
+                    render.DrawBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( r, g, b, math.floor( math.sin( CurTime() * 8 ) * 5 ) + 10 ) )
+                    render.DrawWireframeBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( r, g, b ), false )
 
                     local pos, distance, ang, alpha
                     local eyepos = EyePos()
---
+++ zs_exfil_tool/lua/weapons/gmod_tool/stools/exfil.lua
@@ -0,0 +1,357 @@
+TOOL.Category = "ZS - FWKZT"
+TOOL.Name = "#tool.exfil.name"
+
+if ( CLIENT ) then
+	language.Add( "tool.exfil.name", "Exfil Tool" )
+	language.Add( "tool.exfil.desc", "Exfil area creation tool" )
+
+	language.Add( "tool.exfil.left", "Start new exfil area" )
+	language.Add( "tool.exfil.left_1_1", "Choose area size" )
+	language.Add( "tool.exfil.left_1_2", "Choose area height" )
+
+	language.Add( "tool.exfil.left_2_1", "Edit selected area" )
+
+	language.Add( "tool.exfil.right", "Select existing area" )
+	language.Add( "tool.exfil.right_1_1", "Step back" )
+
+	language.Add( "tool.exfil.right_2_1", "Reselect/Cancel selection" )
+
+	language.Add( "tool.exfil.reload", "Set position" )
+
+	language.Add( "tool.exfil.delete", "Delete Selection" )
+
+	language.Add( "tool.exfil.name", "Exfil Name" )
+
+	
+	language.Add( "tool.exfil.slaydelay", "Zombie slay delay" )
+	language.Add( "tool.exfil.slaydelay.help", "How long to wait until slaying zombies" )
+	
+	language.Add( "tool.exfil.time", "Time to escape" )
+	language.Add( "tool.exfil.time.help", "Aamount of time to escape once in area" )
+	
+	language.Add( "tool.exfil.deadline", "Exfil deadline" )
+	language.Add( "tool.exfil.deadline.help", "Allowed time to get to exfil and escape" )
+	
+	language.Add( "tool.exfil.usehatch", "Use escape hatch?" )
+	
+	language.Add( "tool.exfil.override", "Override?" )
+	language.Add( "tool.exfil.override.help", "Leave as default." )
+	
+	language.Add( "tool.exfil.apply", "Apply To Selection" )
+
+	language.Add( "tool.exfil.save", "Save File" )
+end
+
+TOOL.ClientConVar["editmode"] = "0"
+TOOL.ClientConVar["cx"] = "0"
+TOOL.ClientConVar["cy"] = "0"
+TOOL.ClientConVar["cz"] = "0"
+TOOL.ClientConVar["px"] = "0"
+TOOL.ClientConVar["py"] = "0"
+TOOL.ClientConVar["pz"] = "0"
+TOOL.ClientConVar["menu"] = "0"
+TOOL.ClientConVar["slaydelay"] = "0"
+TOOL.ClientConVar["time"] = "0"
+TOOL.ClientConVar["deadline"] = "0"
+TOOL.ClientConVar["usehatch"] = "0"
+TOOL.ClientConVar["override"] = "0"
+
+TOOL.Information = {
+	{ name = "left", stage = 0 },
+	{ name = "left_2_1", stage = 2 },
+	{ name = "left_1_1", stage = 1, op = 1 },
+	{ name = "left_1_2", stage = 1, op = 2 },
+	{ name = "right", stage = 0 },
+	{ name = "right_1_1", stage = 1 },
+	{ name = "right_2_1", stage = 2 },
+	{ name = "reload", stage = 1 },
+	{ name = "reload", stage = 2 },
+}
+
+function TOOL:Deploy()
+	if CLIENT then return end
+	local owner = self:GetOwner()
+	owner:ConCommand("exfil_editmode 0")
+	self:SetOperation(1)
+	self:SetStage(0)
+	owner:ConCommand("exfil_select \"\"")
+	if self.ExfilCreated then
+		owner:ConCommand("exfil_delete")
+	end
+end
+
+function TOOL:Holster()
+	if CLIENT then return end
+	local owner = self:GetOwner()
+	owner:ConCommand("exfil_editmode 0")
+	self:SetOperation(1)
+	self:SetStage(0)
+	owner:ConCommand("exfil_select \"\"")
+	if self.ExfilCreated then
+		owner:ConCommand("exfil_delete")
+	end
+end
+
+function TOOL:LeftClick( trace )
+	if CLIENT then return end
+
+	local owner = self:GetOwner()
+	
+	if owner:GetInfo("exfil_select") == "" then
+		self:SetStage(0)
+	end
+
+	if self:GetStage() == 1 then
+		if self:GetOperation() == 1 then
+			owner:ConCommand("exfil_cx " .. trace.HitPos.x - self:GetClientNumber("px", 0))
+			owner:ConCommand("exfil_cy " .. trace.HitPos.y - self:GetClientNumber("py", 0))
+			
+			owner:ConCommand("exfil_editmode 2")
+
+			self:SetOperation(2)
+
+			return true
+		elseif self:GetOperation() == 2 then
+			local cz = trace.HitPos.z - self:GetClientNumber("pz", 0)
+			owner:ConCommand("exfil_cz " .. cz)
+			
+			owner:ConCommand("exfil_size " .. owner:GetInfo("exfil_select") .. " " .. self:GetClientNumber("cx", 25) .. " " .. self:GetClientNumber("cy", 25) .. " " .. cz)
+
+			owner:ConCommand("exfil_editmode 0")
+			
+			owner:ConCommand("exfil_select \"\"")
+
+			self.ExfilCreated = false
+
+			self:SetStage(0)
+			
+			return true
+		end
+	end
+	
+	if self:GetStage() == 0 then
+		self.ExfilCreated = true
+		owner:ConCommand("exfil_create")
+		owner:ConCommand("exfil_origin")
+		
+		owner:ConCommand("exfil_editmode 1")
+
+		owner:ConCommand("exfil_px " .. trace.HitPos.x)
+		owner:ConCommand("exfil_py " .. trace.HitPos.y)
+		owner:ConCommand("exfil_pz " .. trace.HitPos.z)
+
+		self:SetStage(1)
+		self:SetOperation(1)
+	end
+
+	if self:GetStage() == 2 then
+		owner:ConCommand("exfil_editmode 1")
+
+		self:SetStage(1)
+		self:SetOperation(1)
+	end
+
+	return true
+end
+
+function TOOL:RightClick( trace )
+	if CLIENT then return end
+	local owner = self:GetOwner()
+	if owner:GetInfo("exfil_select") == "" then
+		self:SetStage(0)
+	end
+	if self:GetStage() == 1 then
+		local editMode = GetConVar("exfil_editmode"):GetInt()
+		if editMode > 0 then
+			owner:ConCommand("exfil_editmode " .. editMode - 1)
+	
+			if self:GetOperation() == 2 then
+				self:SetOperation(1)
+			elseif self:GetOperation() == 1 then
+				self:SetStage(0)
+	
+				if self.ExfilCreated then
+					owner:ConCommand("exfil_delete")
+				end
+			end
+		end
+		return
+	end
+
+	owner:ConCommand("exfil_select_closest")
+	self:SetStage(2)
+
+	timer.Simple(0, function()
+		if owner:GetInfo("exfil_select") == "" then
+			self:SetStage(0)
+		end
+	end)
+end
+
+function TOOL:Reload( trace )
+	if CLIENT then return end
+	local owner = self:GetOwner()
+	if owner:GetInfo("exfil_select") == "" then
+		self:SetStage(0)
+	end
+	if self:GetStage() >= 1 then
+		local owner = self:GetOwner()
+		owner:ConCommand("exfil_pos " .. owner:GetInfo("exfil_select") .. " " .. trace.HitPos.x .. " " .. trace.HitPos.y .. " " .. trace.HitPos.z)
+		owner:ConCommand("exfil_px " .. trace.HitPos.x)
+		owner:ConCommand("exfil_py " .. trace.HitPos.y)
+		owner:ConCommand("exfil_pz " .. trace.HitPos.z)
+	end
+end
+
+-- This function/hook is called every frame on client and every tick on the server
+function TOOL:Think()
+	if CLIENT then return end
+end
+
+local function ReconstructListView(listView)
+	listView:Clear()
+
+	for _, entry in ipairs(ExfilStorage[game.GetMap()]) do
+		local name = table.GetKeys(entry)[1]
+		local line = listView:AddLine(name)
+		line.data = { exfil_select = name, exfil_menu = "1" }
+
+		if GetConVar("exfil_select"):GetString() == name then line:SetSelected(true) end
+	end
+end
+
+function TOOL.BuildCPanel( CPanel )
+	LocalPlayer():ConCommand("exfil_show 1")
+
+	local exfilMenu
+
+	local namePanel = vgui.Create("DTextEntry", CPanel)
+	namePanel:SetPlaceholderText(GetConVar("exfil_select"):GetString())
+	namePanel.OnEnter = function(self)
+		local old = GetConVar("exfil_select"):GetString()
+		local new = self:GetValue()
+		self:SetText("")
+		if old == new then return end
+
+		for i, data in ipairs( ExfilStorage[ game.GetMap() ] ) do
+			for area, stored in pairs( data ) do
+				if area == new then
+					return
+				end
+				if area == old then
+					ExfilStorage[ game.GetMap() ][ i ][ old ] = nil
+					ExfilStorage[ game.GetMap() ][ i ][ new ] = stored
+				end
+				self:SetPlaceholderText(new)
+			end
+		end
+
+		ReconstructListView(exfilMenu)
+	end
+
+	local delete = CPanel:AddControl("Button", { Label = "#tool.exfil.delete" })
+	delete.DoClick = function(self)
+		LocalPlayer():ConCommand("exfil_delete")
+		exfilMenu:RemoveLine(exfilMenu:GetSelectedLine())
+	end
+
+	exfilMenu = CPanel:AddControl("ListBox", { Label = "#tool.exfil.name", Height = 17 * 25 })
+	exfilMenu:SetMultiSelect(false)
+	ReconstructListView(exfilMenu)
+
+	CPanel:AddPanel(namePanel)
+
+	CPanel:AddControl("Slider", { Label = "#tool.exfil.slaydelay", Type = "Int", Min = 0, Max = 60, Help = true, Command = "exfil_slaydelay" })
+	CPanel:AddControl("Slider", { Label = "#tool.exfil.time", Type = "Int", Min = 0, Max = 60, Help = true, Command = "exfil_time" })
+	CPanel:AddControl("Slider", { Label = "#tool.exfil.deadline", Type = "Int", Min = 0, Max = 300, Help = true, Command = "exfil_deadline" })
+	CPanel:AddControl("CheckBox", { Label = "#tool.exfil.usehatch", Help = false, Command = "exfil_usehatch" })
+	CPanel:AddControl("CheckBox", { Label = "#tool.exfil.override", Help = true, Command = "exfil_override" })
+	
+	CPanel:AddControl("Button", { Label = "#tool.exfil.apply", Command = "exfil_apply" })
+	CPanel:AddControl("Button", { Label = "#tool.exfil.save", Command = "exfil_save" })
+
+	cvars.AddChangeCallback("exfil_select", function(convar, oldValue, newValue)
+		if newValue ~= "" then
+			namePanel:SetPlaceholderText(newValue)
+			ReconstructListView(exfilMenu)
+		end
+	end)
+end
+
+hook.Add("PostDrawTranslucentRenderables", "ExfilToolgunDraw", function(_, bDrawingSkybox)
+	if not GetConVar("exfil_show"):GetBool() then return end
+	if bDrawingSkybox then return end
+	if ExfilStorage[ game.GetMap() ] then
+		for area, data in pairs( ExfilStorage[ game.GetMap() ] ) do
+			for area, stored in pairs( data ) do
+				
+				if GetConVar("exfil_select"):GetString() == area then
+					local editMode = GetConVar("exfil_editmode"):GetInt()
+					if editMode == 0 then
+						local oc = Color( 255, 157, 0 )
+					
+						if GetConVar("exfil_menu"):GetBool() then
+							oc = Color(0, 179, 255)
+						end
+
+						local r = oc.r
+						local g = oc.g
+						local b = oc.b
+						
+						render.SetColorMaterial()
+						render.DrawBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( r, g, b, math.floor( math.sin( CurTime() * 8 ) * 5 ) + 10 ) )
+						render.DrawWireframeBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), stored.BoxSize or Vector(25,25,25), Color( r, g, b ), false )
+
+						local pos, distance, ang, alpha
+						local eyepos = EyePos()
+						combined_vec = stored.BoxSize
+						local vx,vy,vz = combined_vec.x, combined_vec.y, combined_vec.z
+				
+						vx = vx/2
+						vy = vy/2
+						vz = 64
+				
+						pos = stored.Pos+Vector( vx, vy, vz )
+				
+						distance = eyepos:DistToSqr(pos)
+	
+						alpha = math.min(255, math.sqrt(distance / 5))
+						ang = (eyepos - pos):Angle()
+						ang:RotateAroundAxis(ang:Right(), 270)
+						ang:RotateAroundAxis(ang:Up(), 90)
+				
+						cam.IgnoreZ(true)
+						cam.Start3D2D(pos, ang, math.max(1200, math.sqrt(distance)) / 5000)
+						local oldfogmode = render.GetFogMode()
+						render.FogMode(0)
+				
+						surface.SetFont( "Trebuchet24" )
+						surface.SetTextColor( 255, 255, 255, alpha )
+						surface.SetTextPos( 0, 0 ) 
+						surface.DrawText( area )
+				
+						render.FogMode(oldfogmode)
+						cam.End3D2D()
+						cam.IgnoreZ(false)
+					elseif editMode == 1 then
+						local eyeTrace = LocalPlayer():GetEyeTrace()
+						local x, y = eyeTrace.HitPos.x - stored.Pos.x, eyeTrace.HitPos.y - stored.Pos.y
+
+						render.SetColorMaterial()
+						render.DrawWireframeBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), Vector(x, y, 0), Color( 161, 255, 156 ), false )
+						render.DrawLine(Vector(eyeTrace.HitPos.x, eyeTrace.HitPos.y, stored.Pos.z), eyeTrace.HitPos, Color(161, 255, 156), false)
+					elseif editMode == 2 then
+						local eyeTrace = LocalPlayer():GetEyeTrace()
+						local cx = GetConVar("exfil_cx"):GetFloat()
+						local cy = GetConVar("exfil_cy"):GetFloat()
+
+						render.SetColorMaterial()
+						render.DrawBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), Vector(cx, cy, 0), Color( 161, 255, 156, math.floor( math.sin( CurTime() * 8 ) * 5 ) + 10 ) )
+						
+						render.DrawWireframeBox( stored.Pos, Angle(0,0,0), Vector(0,0,0), Vector(cx, cy, eyeTrace.HitPos.z - stored.Pos.z), Color( 161, 255, 156 ), false )
+					end
+				end
+			end
+		end
+	end
+end)
